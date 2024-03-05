8178e13a8bb40c10b16321a58632563df49f6ac1
Iconians <claytoncripe@gmail.com>
2024-03-05 10:36:28 -0800

    redid imports on navbar so all the styled components are in one import rather then several

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,62 +1,22 @@
-import { NavbarWrapper } from "../../styledComponents/Navbar/Navbar";
-import { FlexNav } from "../../styledComponents/Navbar/Navbar";
-import { NavHeader } from "../../styledComponents/Navbar/Navbar";
-import { NavLogo } from "../../styledComponents/Navbar/Navbar";
-import { NavToggle } from "../../styledComponents/Navbar/Navbar";
-import { NavLinks } from "../../styledComponents/Navbar/Navbar";
-import { NavLiLink } from "../../styledComponents/Navbar/Navbar";
 import { NavLink } from "react-router-dom";
 import { navLinks } from "../../utils/navLinks";
 import WayToHealth_Logo from "../../assets/WayToHealth_Logo.svg";
+import {
+  NavbarWrapper,
+  FlexNav,
+  NavHeader,
+  NavLogo,
+  NavToggle,
+  NavLinks,
+  NavLiLink,
+} from "../../styledComponents/Navbar/Navbar";
 import styles from "./Navbar.module.css";
-// import { useState, useRef } from "react";
 
 type NavbarProps = {
   scrollDirection: string;
 };
 
 export const Navbar = ({ scrollDirection }: NavbarProps) => {
-  // const [scrollDirection, setScrollDirection] = useState("scroll-up");
-  // const lastScrollY = useRef(0);
-
-  // const handleScroll = (e: React.UIEvent<HTMLElement>) => {
-  //   const currentScrollY = (e.target as HTMLElement).scrollTop;
-  //   console.log(currentScrollY, lastScrollY.current);
-
-  //   if (currentScrollY > lastScrollY.current) {
-  //     setScrollDirection("scroll-down");
-  //   } else {
-  //     setScrollDirection("scroll-up");
-  //   }
-
-  //   lastScrollY.current = currentScrollY;
-  // };
-  // console.log(scrollDirection, lastScrollY);
-
-  // const [scrollDirection, setScrollDirection] = useState("scroll-up");
-
-  // useEffect(() => {
-  //   let lastScroll = 0;
-
-  //   const handleScroll = () => {
-  //     const currentScroll = window.scrollY;
-  //     if (currentScroll <= 0) {
-  //       setScrollDirection("scroll-up");
-  //     } else if (currentScroll > lastScroll) {
-  //       setScrollDirection("scroll-down");
-  //     } else if (currentScroll < lastScroll) {
-  //       setScrollDirection("scroll-up");
-  //     }
-  //     lastScroll = currentScroll;
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
   return (
     <div className={scrollDirection === "scroll-down" ? styles.scrollDown : ""}>
       <NavbarWrapper>

3cb4ce100b5774924ffa0bf9f185bd240e854476
RShnitser <romanshnitser@hotmail.com>
2024-05-01 19:23:10 -0400

    add pdf files and correct links

--- src/utils/documentLinkValues.ts
+++ src/utils/documentLinkValues.ts
@@ -4,7 +4,7 @@ export type DocumentLink = {
 };
 
 export const documentLinks: DocumentLink[] = [
-    { title: "Profit and Loss 2020", file: "/assets/profit_and_loss_2020.pdf" },
-    { title: "Profit and Loss 2021", file: "/assets/profit_and_loss_2021.pdf" },
-    { title: "Profit and Loss 2022", file: "/assets/profit_and_loss_2022.pdf" },
+    { title: "Profit and Loss 2020", file: "/profit_and_loss/profit_and_loss_2020.pdf" },
+    { title: "Profit and Loss 2021", file: "/profit_and_loss/profit_and_loss_2021.pdf" },
+    { title: "Profit and Loss 2022", file: "/profit_and_loss/profit_and_loss_2022.pdf" },
   ];

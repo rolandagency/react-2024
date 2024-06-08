7a1888a56b3f1983b4a850904faea076b1f7cf6b
RShnitser <romanshnitser@hotmail.com>
2024-06-08 19:49:24 -0400

    add nav and footer to newsletter

--- src/pages/NewsLetter/NewLetter.tsx
+++ src/pages/NewsLetter/NewLetter.tsx
@@ -1,8 +1,19 @@
+import { useContext } from "react";
+import { Navbar } from "../../components/Navbar/Navbar";
+import AppContext from "../../providers/AppContext";
+import { Footer } from "../../components/Footer/Footer";
+
 export const NewLetter = () => {
+  const { scrollDirection, heroSectionRef } = useContext(AppContext);
+
   return (
-    <>
+    <div>
+      <Navbar scrollDirection={scrollDirection} />
       <h1 className="text-3xl font-bold underline">NewLetter</h1>
-      <p>way to health</p>
-    </>
+      <section ref={heroSectionRef}>
+        <p>way to health</p>
+      </section>
+      <Footer />
+    </div>
   );
 };

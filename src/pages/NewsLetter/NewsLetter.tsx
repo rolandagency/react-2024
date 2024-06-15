090836164e6df7a178cd899d6f815bd77375697c
RShnitser <romanshnitser@hotmail.com>
2024-06-15 01:54:05 -0400

    add header styles

--- src/pages/NewsLetter/NewsLetter.tsx
+++ src/pages/NewsLetter/NewsLetter.tsx
@@ -7,8 +7,8 @@ import { articles, events } from "../../utils/newsLetterValues";
 const Article = ({ title, content }: { title: string; content: string }) => {
   return (
     <>
-      <h2>{title}</h2>
-      <p>{content}</p>
+      <h2 className="font-inter font-bold text-4xl my-5">{title}</h2>
+      <p className="text-neutral-200 mb-20">{content}</p>
     </>
   );
 };
@@ -27,14 +27,16 @@ export const NewsLetter = () => {
         className="bg-[#363438] grid grid-cols-2 items-center"
         ref={heroSectionRef}
       >
-        <h1 className="text-center text-3xl font-bold">NewLetter</h1>
+        <h1 className="font-inter text-center text-6xl italic">
+          <span className="text-accent-color">NEWS</span>LETTER
+        </h1>
 
         <div>
-          <img src="newsletter_hero.svg" alt="hero" />
+          <img src="Newsletter_hero.png" alt="hero" />
         </div>
       </section>
       <section className="grid grid-cols-3">
-        <div className="col-span-2">
+        <div className="col-span-2 px-20 pt-20">
           {articles.map((article) => (
             <Article
               key={article.title}
@@ -42,12 +44,11 @@ export const NewsLetter = () => {
               content={article.content}
             />
           ))}
-          <div className="bg-[#29282D] mix-blend-screen">
-            WAY TO HEALTH - JANUARY 2024
-          </div>
         </div>
-        <div className="bg-[#333135]">
-          <h2>Upcoming Events</h2>
+        <div className="bg-[#333135] px-10 pt-20">
+          <h2 className="font-inter text-4xl my-5 text-center">
+            Upcoming Events
+          </h2>
           <div>
             {events.map((ev, index) => (
               <Event key={`event-${index}`} description={ev.description} />
@@ -64,6 +65,11 @@ export const NewsLetter = () => {
             <div>EMAIL</div>
           </div>
         </div>
+        <div className="text-5xl col-span-2 bg-[#29282D] mix-blend-screen py-10 px-16">
+          <span className="text-accent-color">WAY TO</span> HEALTH - JANUARY
+          2024
+        </div>
+        <div className="bg-[#333135] mix-blend-screen"></div>
       </section>
       <Footer />
     </div>
--- src/utils/newsLetterValues.ts
+++ src/utils/newsLetterValues.ts
@@ -16,12 +16,14 @@ export const articles: Article[] = [
             aliquip ex ea commodo consequat. quis nostrud exercitation ullamco
             laboris nisi ut aliquip ex ea commodo consequat. quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo conse
+
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. quis nostrud exercitation ullamco
             laboris nisi ut aliquip ex ea commodo consequat. quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo conse
+            
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut

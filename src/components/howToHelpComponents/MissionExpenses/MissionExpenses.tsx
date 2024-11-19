99ba44182ba7ba8c16f0a39c1af7a98ad76eae01
Tyson <deppravacion@gmail.com>
2024-11-18 22:31:57 -0600

    Refactor: how to help

---
+++ src/components/howToHelpComponents/MissionExpenses/MissionExpenses.tsx
@@ -0,0 +1,24 @@
+export const MissionExpenses = () => {
+  return (
+    <section>
+      <div>
+        <h2 className="mb-6">mission trips</h2>
+        <strong>MISSIONARY EXPENSES</strong>
+        <p className="mb-6">
+          A common question: what does it cost to go on a mission trip? Here is
+          a price breakdown per missionary. During an 6-day mission trip,
+          missionaries typically incur an estimated cost of $760 for the trip
+          itself. Additionally, airfare expenses range from $600 to $800
+          round-trip. Missionaries participating in our mission trips are
+          relieved of the financial burden associated with various expenses. The
+          foundation covers all work related costs, including travel to clinics,
+          medical staff pay, translator pay, security pay, and the expenses
+          incurred for medications used in clinics, and any additional patient
+          treatments. The fundraising before each trip allows our missionaries
+          to focus solely on the important work of the foundation without having
+          to worry about these additional financial responsibilities.
+        </p>
+      </div>
+    </section>
+  );
+};
